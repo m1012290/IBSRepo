a820c88f5724146d83cc3df5ea3e25607da8f487
M1014970 <M1014970@A2ML10140.mindtree.com>
2014-04-19 16:07:13 +0530

    User get based on UserID and login name

--- IBSServices/src/com/shrinfo/ibs/userdets/dao/UserDetailsDaoImpl.java
+++ IBSServices/src/com/shrinfo/ibs/userdets/dao/UserDetailsDaoImpl.java
@@ -25,118 +25,139 @@ import com.shrinfo.ibs.vo.business.UserRoleVO;
 
 /**
  * @author Sunil kumar
- *
+ * 
  */
-public class UserDetailsDaoImpl extends BaseDBDAO implements UserDetailsDao{
+public class UserDetailsDaoImpl extends BaseDBDAO implements UserDetailsDao {
 
-	@Override
-	public BaseVO getUserDetails(BaseVO baseVO) {
-		if( Utils.isEmpty(baseVO)){
-            throw new BusinessException("userdetails.nullinput", null, "Input recieved for user details ["+baseVO+"] is null");
+    @Override
+    public BaseVO getUserDetails(BaseVO baseVO) {
+        if (Utils.isEmpty(baseVO)) {
+            throw new BusinessException("userdetails.nullinput", null,
+                "Input recieved for user details [" + baseVO + "] is null");
         }
-		
-		UserVO userVO = (UserVO)baseVO;
-		if(Utils.isEmpty(userVO.getLoginName())){
-            throw new BusinessException("userdetails.nullinput", null, "Input recieved for user details ["+baseVO+"] is null");
+
+        UserVO userVO = (UserVO) baseVO;
+        if (Utils.isEmpty(userVO.getLoginName()) && Utils.isEmpty(userVO.getUserId())) {
+            throw new BusinessException("userdetails.nullinput", null,
+                "Input recieved for user details [" + baseVO + "] is null");
         }
-		
-		if(Utils.isEmpty(userVO.getLoginName())){
-			throw new BusinessException("userdetails.nullinput", null, "Input recieved for user details ["+baseVO+"] is null");
-		}
-		List<IbsUser> ibsUserList = queryUserDetails(userVO);
-		if(Utils.isEmpty(ibsUserList)){
-			throw new BusinessException("userdetails.invalidinput", null, "Invalid user details recieved, please check the same");
-		}
-		try{
-		    populateUserDetails(ibsUserList, userVO);
-		}catch(Exception ex){
-		    System.out.println("Exception encountered while populating user details");
-		    throw new SystemException(ex, "Exception ["+ex+"] while populating user details");
-		}
-		return baseVO;
-	}
-	
-	@SuppressWarnings("unchecked")
-	private List<IbsUser> queryUserDetails(UserVO userVO){
-		//getHibernateTemplate().findByNamedParam("from IbsMasLookup ibsMasLookUp where ibsMasLookUp.id.category=:identifier", "identifier", categoryName);
-		return getHibernateTemplate().findByNamedParam("from IbsUser ibsUser where ibsUser.loginName=:loginName", "loginName", userVO.getLoginName());
-	}
-	//Update userVO with loginname, password details
-	private void populateUserDetails(List<IbsUser> ibsUserList, UserVO userVO){
-		IBSUserVO ibsUser = (IBSUserVO) userVO;
-		userVO.setIsActive(ibsUserList.get(0).getStatus());
-		userVO.setPassword(ibsUserList.get(0).getPassword());
-		userVO.setLoginName(ibsUserList.get(0).getLoginName());
-		userVO.setUserId(ibsUserList.get(0).getId());
-		//user contact details
-		populateUserContact(ibsUserList.get(0), ibsUser);
-		//user branch details
-		populateUserBranch(ibsUserList.get(0), ibsUser);
-		//user roles along with products applicable for each of the roles
-		populateUserRoles(ibsUserList.get(0), ibsUser);
-	}
-	
-	private void populateUserContact(IbsUser ibsUser, UserVO userVO){
-	    IBSUserVO ibsUserVO = (IBSUserVO) userVO;
-	    ContactVO contactDetails = new ContactVO();
-	    contactDetails.setContactId(ibsUser.getIbsContact().getId());
-	    contactDetails.setEmailId(ibsUser.getIbsContact().getPrimaryEmailId());
-	    contactDetails.setLandlineNo(ibsUser.getIbsContact().getPrimaryLandlineNo());
-	    contactDetails.setMobileNo(ibsUser.getIbsContact().getPrimaryMobileNo());
-	    contactDetails.setFirstName(ibsUser.getIbsContact().getFirstName());
-	    contactDetails.setMiddleName(ibsUser.getIbsContact().getMiddleName());
-	    contactDetails.setLastName(ibsUser.getIbsContact().getLastName());
-	    contactDetails.setTitle(ibsUser.getIbsContact().getTitle());
-	    AddressVO addressVO = new AddressVO();
-	    addressVO.setCity(ibsUser.getIbsContact().getCity());
-	    addressVO.setCountry(ibsUser.getIbsContact().getCountry());
-	    addressVO.setPoBox(ibsUser.getIbsContact().getPobox());
-	    addressVO.setAddress(ibsUser.getIbsContact().getAddress());
-	    addressVO.setLattitude(Utils.isEmpty(ibsUser.getIbsContact().getLocationLattitude())?null:String.valueOf(ibsUser.getIbsContact().getLocationLattitude()));
-	    addressVO.setLongitude(Utils.isEmpty(ibsUser.getIbsContact().getLocationLongitude())?null:String.valueOf(ibsUser.getIbsContact().getLocationLongitude()));
-	    contactDetails.setAddressVO(addressVO);
-	    ibsUserVO.setContactDetails(contactDetails);
-	}
-	
-	private void populateUserBranch(IbsUser ibsUser, UserVO userVO){
-	    IBSUserVO ibsUserVO = (IBSUserVO) userVO;
-	    BranchVO branchVO = new BranchVO();
-	    branchVO.setCode(ibsUser.getIbsCompanyBranch().getCode());
-	    branchVO.setName(ibsUser.getIbsCompanyBranch().getName());
-	    branchVO.setAbbrevation(ibsUser.getIbsCompanyBranch().getAbbr());
-	    ibsUserVO.setBranchDetails(branchVO);
-	}
-	
-	private void populateUserRoles(IbsUser ibsUser, UserVO userVO){
-	    IBSUserVO ibsUserVO = (IBSUserVO) userVO;
-	    List<UserRoleVO> roles = new ArrayList<UserRoleVO>();
-	    List<UserRolePrivilege> rolesProductPrivileges = new ArrayList<UserRolePrivilege>();
-	    UserRoleVO userRoleVO =  null;
-	    for(IbsUserRoleLink userRolesLink : ibsUser.getIbsUserRoleLinks()){
-	        userRoleVO = new UserRoleVO();
-	        userRoleVO.setId(userRolesLink.getIbsRoles().getId());
-	        userRoleVO.setDesc(userRolesLink.getIbsRoles().getDescription());
-	        userRoleVO.setName(userRolesLink.getIbsRoles().getName());
-	        populateUserRolePrivilege(userRolesLink.getIbsRoles(), rolesProductPrivileges);
-	        userRoleVO.setRoleProductPrivileges(rolesProductPrivileges);
-	        roles.add(userRoleVO);
-	    }
-	    ibsUserVO.setRoles(roles);
-	}
-	
-	private void populateUserRolePrivilege(IbsRoles roles, List<UserRolePrivilege> userRolePrivileges){
-	    UserRolePrivilege userRolePrivilege = null;
-	    ProductVO productVO = null;
-	    Iterator<IbsRolesProductPrivileges> rolesProductPrivilege = roles.getIbsRolesProductPrivilegeses().iterator();
-	    while(rolesProductPrivilege.hasNext()){
-	        userRolePrivilege = new UserRolePrivilege();
-	        IbsRolesProductPrivileges roleProductPrivileges = rolesProductPrivilege.next();
-	        userRolePrivilege.setEmailQuoteSlip(roleProductPrivileges.getEmailQuoteslip());
-	        userRolePrivilege.setEmailRequisition(roleProductPrivileges.getEmailRequisition());
-	        userRolePrivilege.setGenerateQuoteSlip(roleProductPrivileges.getGenerateQuoteslip());
-	        userRolePrivilege.setEmailClosingSlip(roleProductPrivileges.getEmailClosingslip());
-	        userRolePrivilege.setGenerateClosingSlip(roleProductPrivileges.getGenerateClosingslip());
-	        userRolePrivilege.setFreeText1(roleProductPrivileges.getFreetext1());
+        List<IbsUser> ibsUserList = null;
+        if (!Utils.isEmpty(userVO.getUserId())) {
+            ibsUserList = queryUserDetailsByUserId(userVO);
+        } else if (!Utils.isEmpty(userVO.getLoginName())) {
+            ibsUserList = queryUserDetailsByLoginName(userVO);
+        }
+
+        if (Utils.isEmpty(ibsUserList)) {
+            throw new BusinessException("userdetails.invalidinput", null,
+                "Invalid user details recieved, please check the same");
+        }
+        try {
+            populateUserDetails(ibsUserList, userVO);
+        } catch (Exception ex) {
+            System.out.println("Exception encountered while populating user details");
+            throw new SystemException(ex, "Exception [" + ex + "] while populating user details");
+        }
+        return baseVO;
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<IbsUser> queryUserDetailsByLoginName(UserVO userVO) {
+        // getHibernateTemplate().findByNamedParam("from IbsMasLookup ibsMasLookUp where ibsMasLookUp.id.category=:identifier",
+        // "identifier", categoryName);
+        return getHibernateTemplate().findByNamedParam(
+            "from IbsUser ibsUser where ibsUser.loginName=:loginName", "loginName",
+            userVO.getLoginName());
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<IbsUser> queryUserDetailsByUserId(UserVO userVO) {
+        return getHibernateTemplate().findByNamedParam("from IbsUser ibsUser where ibsUser.id=:id",
+            "id", userVO.getUserId());
+    }
+
+    // Update userVO with loginname, password details
+    private void populateUserDetails(List<IbsUser> ibsUserList, UserVO userVO) {
+        IBSUserVO ibsUser = (IBSUserVO) userVO;
+        userVO.setIsActive(ibsUserList.get(0).getStatus());
+        userVO.setPassword(ibsUserList.get(0).getPassword());
+        userVO.setLoginName(ibsUserList.get(0).getLoginName());
+        userVO.setUserId(ibsUserList.get(0).getId());
+        userVO.setUserName(ibsUserList.get(0).getDescription());
+        // user contact details
+        populateUserContact(ibsUserList.get(0), ibsUser);
+        // user branch details
+        populateUserBranch(ibsUserList.get(0), ibsUser);
+        // user roles along with products applicable for each of the roles
+        populateUserRoles(ibsUserList.get(0), ibsUser);
+    }
+
+    private void populateUserContact(IbsUser ibsUser, UserVO userVO) {
+        IBSUserVO ibsUserVO = (IBSUserVO) userVO;
+        ContactVO contactDetails = new ContactVO();
+        contactDetails.setContactId(ibsUser.getIbsContact().getId());
+        contactDetails.setEmailId(ibsUser.getIbsContact().getPrimaryEmailId());
+        contactDetails.setLandlineNo(ibsUser.getIbsContact().getPrimaryLandlineNo());
+        contactDetails.setMobileNo(ibsUser.getIbsContact().getPrimaryMobileNo());
+        contactDetails.setFirstName(ibsUser.getIbsContact().getFirstName());
+        contactDetails.setMiddleName(ibsUser.getIbsContact().getMiddleName());
+        contactDetails.setLastName(ibsUser.getIbsContact().getLastName());
+        contactDetails.setTitle(ibsUser.getIbsContact().getTitle());
+        AddressVO addressVO = new AddressVO();
+        addressVO.setCity(ibsUser.getIbsContact().getCity());
+        addressVO.setCountry(ibsUser.getIbsContact().getCountry());
+        addressVO.setPoBox(ibsUser.getIbsContact().getPobox());
+        addressVO.setAddress(ibsUser.getIbsContact().getAddress());
+        addressVO.setLattitude(Utils.isEmpty(ibsUser.getIbsContact().getLocationLattitude()) ? null
+            : String.valueOf(ibsUser.getIbsContact().getLocationLattitude()));
+        addressVO.setLongitude(Utils.isEmpty(ibsUser.getIbsContact().getLocationLongitude()) ? null
+            : String.valueOf(ibsUser.getIbsContact().getLocationLongitude()));
+        contactDetails.setAddressVO(addressVO);
+        ibsUserVO.setContactDetails(contactDetails);
+    }
+
+    private void populateUserBranch(IbsUser ibsUser, UserVO userVO) {
+        IBSUserVO ibsUserVO = (IBSUserVO) userVO;
+        BranchVO branchVO = new BranchVO();
+        branchVO.setCode(ibsUser.getIbsCompanyBranch().getCode());
+        branchVO.setName(ibsUser.getIbsCompanyBranch().getName());
+        branchVO.setAbbrevation(ibsUser.getIbsCompanyBranch().getAbbr());
+        ibsUserVO.setBranchDetails(branchVO);
+    }
+
+    private void populateUserRoles(IbsUser ibsUser, UserVO userVO) {
+        IBSUserVO ibsUserVO = (IBSUserVO) userVO;
+        List<UserRoleVO> roles = new ArrayList<UserRoleVO>();
+        List<UserRolePrivilege> rolesProductPrivileges = new ArrayList<UserRolePrivilege>();
+        UserRoleVO userRoleVO = null;
+        for (IbsUserRoleLink userRolesLink : ibsUser.getIbsUserRoleLinks()) {
+            userRoleVO = new UserRoleVO();
+            userRoleVO.setId(userRolesLink.getIbsRoles().getId());
+            userRoleVO.setDesc(userRolesLink.getIbsRoles().getDescription());
+            userRoleVO.setName(userRolesLink.getIbsRoles().getName());
+            populateUserRolePrivilege(userRolesLink.getIbsRoles(), rolesProductPrivileges);
+            userRoleVO.setRoleProductPrivileges(rolesProductPrivileges);
+            roles.add(userRoleVO);
+        }
+        ibsUserVO.setRoles(roles);
+    }
+
+    private void populateUserRolePrivilege(IbsRoles roles,
+            List<UserRolePrivilege> userRolePrivileges) {
+        UserRolePrivilege userRolePrivilege = null;
+        ProductVO productVO = null;
+        Iterator<IbsRolesProductPrivileges> rolesProductPrivilege =
+            roles.getIbsRolesProductPrivilegeses().iterator();
+        while (rolesProductPrivilege.hasNext()) {
+            userRolePrivilege = new UserRolePrivilege();
+            IbsRolesProductPrivileges roleProductPrivileges = rolesProductPrivilege.next();
+            userRolePrivilege.setEmailQuoteSlip(roleProductPrivileges.getEmailQuoteslip());
+            userRolePrivilege.setEmailRequisition(roleProductPrivileges.getEmailRequisition());
+            userRolePrivilege.setGenerateQuoteSlip(roleProductPrivileges.getGenerateQuoteslip());
+            userRolePrivilege.setEmailClosingSlip(roleProductPrivileges.getEmailClosingslip());
+            userRolePrivilege
+                    .setGenerateClosingSlip(roleProductPrivileges.getGenerateClosingslip());
+            userRolePrivilege.setFreeText1(roleProductPrivileges.getFreetext1());
             userRolePrivilege.setFreeText2(roleProductPrivileges.getFreetext2());
             userRolePrivilege.setMaxSiLimit(roleProductPrivileges.getToSiLimit());
             userRolePrivilege.setMinSiLimit(roleProductPrivileges.getFromSiLimit());
@@ -144,6 +165,6 @@ public class UserDetailsDaoImpl extends BaseDBDAO implements UserDetailsDao{
             MapperUtil.populateProductVO(productVO, roleProductPrivileges.getIbsProductMaster());
             userRolePrivilege.setProduct(productVO);
             userRolePrivileges.add(userRolePrivilege);
-	    }
-	}
+        }
+    }
 }
